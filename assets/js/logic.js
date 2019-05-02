0036006598c306f99b631c2f87a671cffe5e2347
Joel Gaeta <joelsgaeta@gmail.com>
2019-05-02 14:33:36 -0400

    ajax merged with logic

--- assets/js/logic.js
+++ assets/js/logic.js
@@ -1,109 +1,112 @@
 // mapping of known genres/spirits
-const resultsArr = [{
-        genre: "thiller",
-        spirit: "bourbon",
-    },
-    {
-        genre: "comedy",
-        spirit: "beer",
-    },
-    {
-        genre: "romance",
-        spirit: "red wine",
-    },
-    {
-        genre: "horror",
-        spirit: "gin",
-    },
-    {
-        genre: "drama",
-        spirit: "tequila",
-    },
-    {
-        genre: "crime",
-        spirit: "scotch",
-    },
-    {
-        genre: "fantasy",
-        spirit: "rum",
-    },
-    {
-        genre: "mystery",
-        spirit: "whiskey",
-    },
-    {
-        genre: "family",
-        spirit: "juice",
-    },
-    {
-        genre: "musical",
-        spirit: "champagne",
-    },
+const resultsArr = [
+  {
+    genre: "thriller",
+    spirit: "bourbon"
+  },
+  {
+    genre: "comedy",
+    spirit: "beer"
+  },
+  {
+    genre: "romance",
+    spirit: "red wine"
+  },
+  {
+    genre: "horror",
+    spirit: "gin"
+  },
+  {
+    genre: "drama",
+    spirit: "tequila"
+  },
+  {
+    genre: "crime",
+    spirit: "scotch"
+  },
+  {
+    genre: "fantasy",
+    spirit: "rum"
+  },
+  {
+    genre: "mystery",
+    spirit: "whiskey"
+  },
+  {
+    genre: "family",
+    spirit: "juice"
+  },
+  {
+    genre: "musical",
+    spirit: "champagne"
+  }
 ];
 
 // =======================================================================
 // BEGIN CORE LOGIC
 // =======================================================================
 
-function search() {
-    clearDisplay();
+function search(event) {
+  clearDisplay();
 
-    // prevents submit button from trying to submit form
-    event.preventDefault();
+  // prevents submit button from trying to submit form
+  event.preventDefault();
 
-    // movie user entered 
-    // TODO what if movie is empty?
-    const movie = $("#search-input").val();
+  // movie user entered
+  // TODO what if movie is empty?
+  const movie = $("#search-input").val();
+  console.log(movie);
+  console.log("anything" + event.target);
 
-    // this is a function that performs the API call to OMDB
-    let response = OMDBquery(movie);
+  // this is a function that performs the API call to OMDB
+  let response = OMDBquery(movie);
 
-    // If response is found in OMDB
-    if (response) {
-        // TODO get the genre from the response
-        let genre = undefined;
+  // If response is found in OMDB
+  if (response) {
+    // TODO get the genre from the response
+    let genre = undefined;
 
-        if (genre) {
-            // movie/genre is good - update the div with the movie information
-            updateMovieDiv(response);
+    if (genre) {
+      // movie/genre is good - update the div with the movie information
+      updateMovieDiv(response);
 
-            let spirit = getSpiritForGenre(genre.toLowerCase());
-            if (spirit) {
-                // this is a function that performs the API call to CocktailDB
-                let response = cocktailDBquery(spirit);
+      let spirit = getSpiritForGenre(genre.toLowerCase());
+      if (spirit) {
+        // this is a function that performs the API call to CocktailDB
+        let response = cocktailDBquery(spirit);
 
-                // If response is found in CocktailDB
-                if (response) {
-                    updateDrinkDiv(response);
-                } else {
-                    // error case
-                    let message = `Drink not found for ${spirit}`;
-                    updateErrorDiv(message);
-                }
-            } else {
-                // error case
-                let message = `Spirit not found for ${genre}`;
-                updateErrorDiv(message);
-            }
+        // If response is found in CocktailDB
+        if (response) {
+          updateDrinkDiv(response);
         } else {
-            // error case
-            let message = `Genre not found for ${movie}`;
-            updateErrorDiv(message);
+          // error case
+          let message = `Drink not found for ${spirit}`;
+          updateErrorDiv(message);
         }
-    } else {
+      } else {
         // error case
-        let message = `${movie} was not found in OMDB`;
+        let message = `Spirit not found for ${genre}`;
         updateErrorDiv(message);
+      }
+    } else {
+      // error case
+      let message = `Genre not found for ${movie}`;
+      updateErrorDiv(message);
     }
+  } else {
+    // error case
+    let message = `${movie} was not found in OMDB`;
+    updateErrorDiv(message);
+  }
 }
 
 function getSpiritForGenre(genreData) {
-    for (let i = 0; i < resultsArr.length; i++) {
-        if (genreData == resultsArr[i].genre) {
-            return resultsArr[i].spirit;
-        }
+  for (let i = 0; i < resultsArr.length; i++) {
+    if (genreData == resultsArr[i].genre) {
+      return resultsArr[i].spirit;
     }
-    return undefined;
+  }
+  return undefined;
 }
 
 // =======================================================================
@@ -115,35 +118,55 @@ function getSpiritForGenre(genreData) {
 // =======================================================================
 
 function OMDBquery(movie) {
-    const queryURL = "https://www.omdbapi.com/?t=" + movie + "&apikey=trilogy";
+  const queryURL =
+    "https://www.omdbapi.com/?t=" + movie + "=&plot=short&apikey=trilogy";
 
+  $.ajax({
+    url: queryURL,
+    method: "GET"
+  }).then(function(response) {
+    //var genre = response.Genre.split(", ")[0];
     //this is handling if there is no response back from the API
-    let response = undefined;
-
-    // expectation is one genre per movie
-    // ______________________
-    // TODO ajax call, what do to if movie not found?
-    //_________________________
-    // ==========================================
+    //let response = undefined;
+    console.log(response);
     return response;
+  });
 }
 
 function cocktailDBquery(spirit) {
-    // ==========================================
-    const queryURL = "cocktail db url goes here" + spirit;
+  // ==========================================
+  const drinkURL =
+    "https://www.thecocktaildb.com/api/json/v1/1/filter.php?i=" + spirit;
 
+  $.ajax({
+    url: drinkURL,
+    method: "GET"
+  }).then(function(response1) {
+    const cocktail =
+      response1.drinks[Math.floor(Math.random() * response1.drinks.length)]
+        .strDrink;
+    console.log(cocktail);
+
+    const drinkURL2 =
+      "https://www.thecocktaildb.com/api/json/v1/1/search.php?s=" + cocktail;
+    $.ajax({
+      url: drinkURL2,
+      method: "GET"
+    }).then(function(response2, error) {
+      const ingredient =
+        response2.drinks[Math.floor(Math.random() * response2.drinks.length)]
+          .strIngredient1;
+      console.log(ingredient);
+      // if (error) {
+      //     $("#error-div").text("Drink not found");
+      // }
+
+      return response;
+    });
     //this is handling if there is no response back from the API
     let response = undefined;
-
-    // expectation is multiple drink options per spirit
-    //TODO_________________
-    //ajax call to cocktaildb
-    // ==========================================
-
-
-    return response;
+  });
 }
-
 // =======================================================================
 // END AJAX STUFF
 // =======================================================================
@@ -153,33 +176,44 @@ function cocktailDBquery(spirit) {
 // =======================================================================
 
 function clearDisplay() {
-    //clears inputs from previous searches
-    // $("#search-input").text("");
-    // $("#movie-div").text("");
-    // $("#drink-div").text("");
-    // $("#error-div").text("");
+  //clears inputs from previous searches
+  $("#search-input").text("");
+  $("#movie-div").text("");
+  $("#drink-div").text("");
+  $("#error-div").text("");
 }
 
 function updateMovieDiv(response) {
-    // TODO implement
-    // $("#movie-div").text(the content we build);
+  // TODO implement
+  // $("#movie-div").text(the content we build);
+  var containerDiv = $("#movie-div");
+
+  var titleDiv = $("<h2>").text(response.Title);
+  var yearDiv = $("<div>").text(response.Year);
+  var actorsDiv = $("<div>").text(response.Actors);
+  var plotDiv = $("<p>").text(response.Plot);
+  var genreDiv = $("<div>").text(response.genre);
+
+  containerDiv.append(titleDiv, yearDiv, actorsDiv, genreDiv);
 }
 
 function updateDrinkDiv(response) {
-    // TODO implement
-    // $("#drink-div").text(the content we build);
+  var ContainerDrinkDiv = $("#drink-div");
+
+  var titleDrinkDiv = $("<div>").text(response.Title);
+  var recipeDiv = $("<div>").text(response.recipe);
+  var imgDiv = $("<img>").attr("src", response.img);
 }
 
 function updateErrorDiv(message) {
-    // $("#error-div").text(message);
+  // $("#error-div").text(message);
 }
 
 // on submit search function is run
-$("#submit").on("click", search)
+$("#submit-form").on("submit", search);
 
 // =======================================================================
 // END HTML STUFF
 // =======================================================================
-
 // make sure the display is clear when the page loads
 clearDisplay();
--- projectpink.js
+++ projectpink.js
@@ -1,3 +1,46 @@
+const resultsArr = [
+  {
+    genre: "action",
+    spirit: "bourbon"
+  },
+  {
+    genre: "comedy",
+    spirit: "beer"
+  },
+  {
+    genre: "romance",
+    spirit: "red wine"
+  },
+  {
+    genre: "horror",
+    spirit: "gin"
+  },
+  {
+    genre: "drama",
+    spirit: "tequila"
+  },
+  {
+    genre: "crime",
+    spirit: "scotch"
+  },
+  {
+    genre: "fantasy",
+    spirit: "rum"
+  },
+  {
+    genre: "mystery",
+    spirit: "whiskey"
+  },
+  {
+    genre: "family",
+    spirit: "juice"
+  },
+  {
+    genre: "musical",
+    spirit: "champagne"
+  }
+];
+var spiritName;
 $.ajax({
   url: "https://www.omdbapi.com/?t=inception&y=&plot=short&apikey=trilogy",
   method: "GET"
@@ -11,19 +54,37 @@ $.ajax({
   var genreDiv = $("<div>").text(genre);
 
   containerDiv.append(titleDiv, yearDiv, actorsDiv, genreDiv);
+
+  f1(genre);
 });
-
-$.ajax({
-  url: "https://www.thecocktaildb.com/api/json/v1/1/search.php?i=vodka",
-  method: "GET"
-}).then(function(response) {
-  console.log(response);
-  var containerDiv = $(".container");
-
-  var titleDiv = $("<div>").text(response.Title);
-  var yearDiv = $("<div>").text(response.Year);
-  var actorsDiv = $("<div>").text(response.Actors);
-  var genreDiv = $("<div>").text(response.Genre);
-
-  containerDiv.append(titleDiv, yearDiv, actorsDiv, genreDiv);
-});
+function f1(genre) {
+  console.log(genre);
+  for (let i = 0; i < resultsArr.length; i++) {
+    if (resultsArr[i].genre == genre) {
+      spiritName = resultsArr[i].spirit;
+      console.log(spiritName + "hello");
+    }
+  }
+  const drinkURL =
+    "https://www.thecocktaildb.com/api/json/v1/1/filter.php?i=vodka";
+  $.ajax({
+    url: drinkURL,
+    method: "GET"
+  }).then(function(response1) {
+    const cocktail =
+      response1.drinks[Math.floor(Math.random() * response1.drinks.length)]
+        .strDrink;
+    console.log(cocktail);
+    const drinkURL2 =
+      "https://www.thecocktaildb.com/api/json/v1/1/search.php?s=" + cocktail;
+    $.ajax({
+      url: drinkURL2,
+      method: "GET"
+    }).then(function(response2) {
+      const ingredient =
+        response2.drinks[Math.floor(Math.random() * response2.drinks.length)]
+          .strIngredient1;
+      console.log(ingredient);
+    });
+  });
+}
